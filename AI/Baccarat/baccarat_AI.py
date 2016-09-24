31af1948c0b22da88ae1f624b592efe8ce914468
Harald De Bondt <haralddebondt@gmail.com>
2016-09-24 16:47:18 +0200

    first protoype baccarat NEAT

--- AI/Baccarat/baccarat_AI.py
+++ AI/Baccarat/baccarat_AI.py
@@ -21,44 +21,54 @@ def evaluate(genome):
     net = NEAT.NeuralNetwork()
     genome.BuildPhenotype(net)
 
-    error = 0
+    fitness = 0
 
-    # do stuff and return the fitness
-    net.Flush()
-    net.Input(np.array([1., 0., 1.])) # can input numpy arrays, too
-                                      # for some reason only np.float64 is supported
-    for _ in range(2):
+    #generate 100 games with 8 decks
+    n_games=5
+    n_decks=8
+    bcc_results = baccarat_sim.generate_games(n_games,n_decks)
+
+    for outcome,cards in bcc_results:
+        # net.Flush()
+        #the normalised count cards is number of occurences divided by max (4* nr_of_decks)
+        input =np.append(np.array(list(cards.values()))/(8*4),[1.0]).astype(np.float64)
+        net.Input(input) # can input numpy arrays, too
+                                          # for some reason only np.float64 is supported
         net.Activate()
-    o = net.Output()
-    error += abs(1 - o[0])
+        #bet is either -1,0,1 but output is between 0 and 1
+        output_bet = net.Output()[0] *2 -1
+        bet = round(output_bet)
+        #the error should be differentiable gradient, I think
 
-    net.Flush()
-    net.Input([0, 1, 1])
-    for _ in range(2):
+        fitness += baccarat_sim.payout_map[(bet,outcome)]* (1-abs(bet-output_bet))**2
+    return fitness/len(bcc_results)
+
+def play(genome):
+    net = NEAT.NeuralNetwork()
+    genome.BuildPhenotype(net)
+    money=0
+    n_games=10
+    n_decks=8
+    bcc_results = baccarat_sim.generate_games(n_games,n_decks)
+
+    for outcome,cards in bcc_results:
+        # net.Flush()
+        #the normalised count cards is number of occurences divided by max (4* nr_of_decks)
+        input =np.append(np.array(list(cards.values()))/(8*4),[1.0]).astype(np.float64)
+        net.Input(input) # can input numpy arrays, too
+                                          # for some reason only np.float64 is supported
         net.Activate()
-    o = net.Output()
-    error += abs(1 - o[0])
+        #bet is either -1,0,1 but output is between 0 and 1
+        output_bet = net.Output()[0] *2 -1
+        bet = round(output_bet)
+        #the error should be differentiable gradient, I think
 
-    net.Flush()
-    net.Input([1, 1, 1])
-    for _ in range(2):
-        net.Activate()
-    o = net.Output()
-    error += abs(o[0])
-
-    net.Flush()
-    net.Input([0, 0, 1])
-    for _ in range(2):
-        net.Activate()
-    o = net.Output()
-    error += abs(o[0])
-
-    return (4 - error)**2
-
+        money += baccarat_sim.payout_map[(bet,outcome)]
+    return money
 
 
 params = NEAT.Parameters()
-params.PopulationSize = 150
+params.PopulationSize = 200
 params.DynamicCompatibility = True
 params.WeightDiffCoeff = 4.0
 params.CompatTreshold = 2.0
@@ -68,7 +78,7 @@ params.OldAgeTreshold = 35
 params.MinSpecies = 5
 params.MaxSpecies = 10
 params.RouletteWheelSelection = False
-params.RecurrentProb = 0.0
+params.RecurrentProb = 0.1
 params.OverallMutationRate = 0.8
 
 params.MutateWeightsProb = 0.90
@@ -80,9 +90,9 @@ params.WeightMutationRate = 0.25
 
 params.MaxWeight = 8
 
-params.MutateAddNeuronProb = 0.03
-params.MutateAddLinkProb = 0.05
-params.MutateRemLinkProb = 0.0
+params.MutateAddNeuronProb = 0.1
+params.MutateAddLinkProb = 0.1
+params.MutateRemLinkProb = 0.04
 
 params.MinActivationA  = 4.9
 params.MaxActivationA  = 4.9
@@ -96,37 +106,45 @@ params.CrossoverRate = 0.75  # mutate only 0.25
 params.MultipointCrossoverRate = 0.4
 params.SurvivalRate = 0.2
 
-
+import viz
+import matplotlib.pyplot as plt
 def getbest(i):
 
-    g = NEAT.Genome(0, 3, 0, 1, False, NEAT.ActivationFunction.UNSIGNED_SIGMOID, NEAT.ActivationFunction.UNSIGNED_SIGMOID, 0, params)
+    g = NEAT.Genome(0, 14, 0, 1, False, NEAT.ActivationFunction.UNSIGNED_SIGMOID, NEAT.ActivationFunction.UNSIGNED_SIGMOID, 0, params)
     pop = NEAT.Population(g, params, True, 1.0, i)
     pop.RNG.Seed(i)
-
-    generations = 0
-    for generation in range(1000):
+    generations=100
+    for gen in range(generations):
         genome_list = NEAT.GetGenomeList(pop)
         fitness_list = EvaluateGenomeList_Serial(genome_list, evaluate, display=False)
         NEAT.ZipFitness(genome_list, fitness_list)
 
         pop.Epoch()
-        generations = generation
+        #visualise
         best = max(fitness_list)
-        if best > 15.0:
-            break
+        worst = min(fitness_list)
+        net = NEAT.NeuralNetwork()
+        pop.Species[0].GetLeader().BuildPhenotype(net)
+        img = np.zeros((500, 500, 3), dtype=np.uint8)
+        img += 10
+        viz.DrawPhenotype(img, (0, 0, 500, 500), net )
 
-    return generations
+        cv2.imshow("CPPN", img)
+        cv2.waitKey(1)
+        print("Generation:",gen)
+        print("Highest fitness:", best)
+        print("Lowest fitness", worst)
 
 
+    return pop.GetBestGenome(),pop.GetBestFitnessEver()
 
-gens = []
-for run in range(100):
-    gen = getbest(run)
-    gens += [gen]
-    print('Run:', run, 'Generations to solve XOR:', gen)
-avg_gens = sum(gens) / len(gens)
 
-print('All:', gens)
-print('Average:', avg_gens)
+genome,fitness = getbest(1)
+print('best fitness', fitness)
+nr_of_new_plays=100
+multiplier=0
+for _ in range(nr_of_new_plays):
+    multiplier += play(genome)
+print("Average multiplier made over ",nr_of_new_plays," games :", multiplier/nr_of_new_plays)
 
 
--- AI/Baccarat/baccarat_sim.py
+++ AI/Baccarat/baccarat_sim.py
@@ -2,6 +2,8 @@ import sys
 import os
 import copy
 import random
+from collections import Counter
+from collections import OrderedDict
 
 
 def bacc_score(rank):
@@ -10,75 +12,119 @@ def bacc_score(rank):
     else:
         return int(rank)
 
+
+suite_map = {
+    0: 'Clubs',
+    1: 'Diamonds',
+    2: 'Hearts',
+    3: 'Spades'
+}
+rank_map = {
+    0: 'Ace',
+    1: '2',
+    2: '3',
+    3: '4',
+    4: '5',
+    5: '6',
+    6: '7',
+    7: '8',
+    8: '9',
+    9: '10',
+    10: 'Jack',
+    11: 'Queen',
+    12: 'King'
+
+}
+
+
 def build_deck():
     deck = []
-    for suite in ['Clubs', 'Diamonds', 'Hearts', 'Spades']:
-        for rank in ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']:
+    for suite in suite_map.keys():
+        for rank in rank_map.keys():
             deck.append([rank, suite, bacc_score(rank)])
     return deck
-
-
 
 
 def generate_deck(n=1):
     decks = build_deck() * n
     random.shuffle(decks)
     return decks
-def take_card(decks,count):
-    rank, suit, score = decks.pop()
-    count.append([rank,suit])
+
+
+def take_card(decks, count):
+    rank, suite, score = decks.pop()
+    count.append(rank)
     return score
 
 
 def simulate_game(decks):
-    card_count=[]
-    player = [take_card(decks,card_count)]
-    bank = [take_card(decks,card_count)]
-    player.append(take_card(decks,card_count))
-    bank.append(take_card(decks,card_count))
+    card_count = []
+    player = [take_card(decks, card_count)]
+    bank = [take_card(decks, card_count)]
+    player.append(take_card(decks, card_count))
+    bank.append(take_card(decks, card_count))
 
     player_score = sum(player) % 10
     bank_score = sum(bank) % 10
     if player_score >= 8 or bank_score >= 8:
-        return end_game(player,bank,card_count)
+        return end_game(player, bank), cards_to_occurrences(card_count)
 
     # player draws 3rd card
     if player_score <= 5:
-        pcard = take_card(decks,card_count)
+        pcard = take_card(decks, card_count)
         player.append(pcard)
         # bank rules
-        if pcard in (2,3) and bank_score <= 4:
-            bank.append(take_card(decks,card_count))
-        elif pcard in (4,5) and bank_score <= 5:
-            bank.append(take_card(decks,card_count))
-        elif pcard in (6,7) and bank_score <= 6:
-            bank.append(take_card(decks,card_count))
+        if pcard in (2, 3) and bank_score <= 4:
+            bank.append(take_card(decks, card_count))
+        elif pcard in (4, 5) and bank_score <= 5:
+            bank.append(take_card(decks, card_count))
+        elif pcard in (6, 7) and bank_score <= 6:
+            bank.append(take_card(decks, card_count))
         elif pcard == 8 and bank_score <= 2:
-            bank.append(take_card(decks,card_count))
-        elif pcard in (1,9,0) and bank_score <= 3:
-            bank.append(take_card(decks,card_count))
+            bank.append(take_card(decks, card_count))
+        elif pcard in (1, 9, 0) and bank_score <= 3:
+            bank.append(take_card(decks, card_count))
     elif bank_score <= 5:
-        bank.append(take_card(decks,card_count))
-    return end_game(player,bank,card_count)
+        bank.append(take_card(decks, card_count))
+    return end_game(player, bank), cards_to_occurrences(card_count)
 
 
-def end_game(player,bank,count):
+win_map = {
+    -1: "bank",
+    0: "tie",
+    1: "player"
+}
+#bet win payout
+payout_map = {
+    (-1,-1):0.95,
+    (-1,1):-1,
+    (-1,0): 0,
+    (1,-1):-1,
+    (1,1):1,
+    (1,0):0,
+    (0,-1):-1,
+    (0,1):-1,
+    (0,0):8
+}
+def end_game(player, bank):
     player_score = sum(player) % 10
     bank_score = sum(bank) % 10
     if player_score == bank_score:
-        return "tie",count
+        return 0
     elif player_score > bank_score:
-        return "player",count
-    return "bank",count
+        return 1
+    return -1
 
 
-def generate_games(n_games,n_decks=8,min_deck_size=10):
+def cards_to_occurrences(cards):
+    return OrderedDict(sorted(Counter(cards).items(), key=lambda t: t[0]))
+
+
+def generate_games(n_games, n_decks):
     results = [];
     for i in range(n_games):
         decks = generate_deck(n_decks)
-        while len(decks) > min_deck_size:
+        while len(decks) > 10:
             result = simulate_game(decks)
             results.append(result)
     return results
-
-
